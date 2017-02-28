354353fe1649c94a5355d9e38cd8da9b8cb39212
Mel <belm104@uqo.ca>
2017-02-28 08:33:27 -0500

    Add project files.

---
+++ Connect4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Connect4.Startup))]
+namespace Connect4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
